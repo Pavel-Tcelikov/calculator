88fc41f8d119d42ed52c5c98f3d52096b70f3cf2
Pavel-Tcelikov <57235325+Pavel-Tcelikov@users.noreply.github.com>
2022-01-08 15:18:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from colorama import init
+from colorama import Fore, Back, Style
+
+init()
+
+print(Fore.BLACK)
+print(Back.GREEN)
+
+What = input("Что делаем? (+, -, *, /): ")
+
+print(Back.CYAN)
+
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+
+print(Back.YELLOW)
+
+if What == "+":
+    с = a + b
+    print(с)
+elif What == "-":
+    c = a - b
+    print(c)
+elif What == "*":
+    c = a * b
+    print(c)
+elif What == "/":
+    c = a / b
+    print(c)
+else:
+    print("Выбрана не верная операция!")
+
+input()
